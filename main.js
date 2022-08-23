80ad5320a492dea1667448e2fadc553bfb58dcba
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-08-23 12:12:37 -0300

    Correção na lógica do programa

--- main.js
+++ main.js
@@ -1,42 +1,28 @@
-const carreira = prompt('Você quer seguir na área de Front-End ou Back-End?');
+const carreira = prompt('Você quer seguir na área de Front-End ou Back-End? Digite o nome da área:');
+let linguagem = '';
 
 if (carreira === 'Front-End') {
-  const frontEnd = prompt('Quer aprender React ou Vue?');
+  linguagem = prompt('Quer aprender React ou Vue?');
 } else if (carreira === 'Back-End') {
-  const backEnd = prompt('Quer aprender C# ou Java?');
+  linguagem = prompt('Quer aprender C# ou Java?');
+} else {
+  alert('Você não inseriu uma área válida!')
 }
 
-const especializar = prompt('Você deseja seguir se especializando na sua área? Responda S ou N');
-if (especializar === 'S') {
+const especialidadeOuFullstack = prompt('Você deseja seguir se especializando na sua área ou deseja se tornar Fullstack? Digite "1" para se especializar ou "2" para Fullstack.');
 
-  let i = false;
-  let tecnologia = '';
-  let maisTecnologia = '';
+if (especialidadeOuFullstack == 1) {
+  alert(`Continue se especializando em ${linguagem} para dominar a área de ${carreira}!`);
+} else if (especialidadeOuFullstack == 2) {
+    alert(`Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se você quer se tronar Fullstack!`);
+} else {
+  alert('Você não inseriu um valor válido!');
+};
 
-  while (i = false) {
-    tecnologia = prompt('Quais tecnologias você deseja se especializar?');
-    alert('`${tecnologia}` me parece ser bem legal!');
+let msg = prompt('Tem mais alguma tecnologia que você gostaria de aprender? Digite "ok" em caso positivo.');
 
-    maisTecnologia = prompt('Tem mais alguma tecnologia que você quer se especializar?');
-    alert('${maisTecnologia} me parece ser bem legal!');
-
-  } if (maisTecnologia === 'N') {
-    i = true;
-    alert('Muito bem! São todas tecnologias muito boas de se especializar.');
-  };
-
-} else if (especializar === 'N') {
-  let i = false;
-
-  while (i = false) {
-    let conhecerTecnologia = prompt('Quais novas tecnologias deseja conhecer?');
-    alert('`${conhecerTecnologia}` me parece ser bem legal!');
-
-    let maisConhecerTecnologia = prompt('Tem mais alguma tecnologia que você gostaria de conhecer?');
-    alert('${maisConhecerTecnologia} me parece ser bem legal!');
-
-  } if (maisConhecerTecnologia === 'N') {
-    i = true;
-    alert('Muito bem! São todas tecnologias muito boas de se conhecer.');
-  }
+while (msg === 'ok') {
+  let novaTecnologia = prompt('Qual?');
+  alert(`${novaTecnologia} é realmente uma tecnologia muito legal!`);
+  msg = prompt('Tem mais alguma tecnologia que você gostaria de aprender? Digite "ok" em caso positivo.');
 };
